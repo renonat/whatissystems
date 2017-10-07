921491fbdc7e8fd350ac580406140bec24d66525
renonat <renonatalizio@gmail.com>
2017-10-06 21:41:14 -0400

    Changes for prod building

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  styleUrls: ['app/app.component.css'],
-  templateUrl: 'app/app.component.html'
+  styleUrls: ['app.component.css'],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent  {
   title = 'What is Systems?';
