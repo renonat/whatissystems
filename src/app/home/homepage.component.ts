03beffc17c3631f7d115d83c3ff366116b48b1b4
Reno Natalizio <renonatalizio@gmail.com>
2017-10-21 16:31:45 -0400

    Add responsive font sizes
    Centralize the colors, add in all the pages to the table of contents.

--- src/app/home/homepage.component.ts
+++ src/app/home/homepage.component.ts
@@ -11,10 +11,25 @@ export class HomepageComponent implements OnInit {
   title = 'What is Systems?'
 
   contents: [ContentLink] = [
-    new ContentLink('What do we learn?', '#/our-education', 'A description', 'rgba(40, 203, 252, 0.84)'),
-    new ContentLink('Is this general engineering?', '#/general-engineering', 'A description', 'rgba(239, 94, 111, 0.84)'),
-    new ContentLink('Is this the right fit for me?', '#/the-right-fit', 'A description', 'rgba(221, 104, 244, 0.84)'),
-    new ContentLink('What do we learn?', '#/our-education', 'A description', 'rgba(40, 203, 252, 0.84)')
+    new ContentLink('What do we learn?',
+      'Our education exists at the intersection of Systems Theory and Design Thinking.',
+      '#/our-education', 'rgba(40, 203, 252, 0.74)'), // blue
+    new ContentLink('Is this a general engineering program?',
+      'SYDE is its own speciality, and has great opportunities for specialization' ,
+      '#/general-engineering', 'rgba(239, 94, 111, 0.74)'), // red
+    new ContentLink('Is this some newfangled program?', 'Learn about the history of our program.',
+      '#', 'rgba(2, 184, 117, 0.74)'), // green
+    new ContentLink('Will this be the right fit for me?',
+      'Explore what it means to be a SYDE student, and the community we have created.',
+      '#/right-fit', 'rgba(221, 104, 244, 0.74)'), // purple
+    new ContentLink('What is co-op like?', 'Everything from 4-stream to rankings, this is how to get a job 101',
+      '#', 'rgba(70, 136, 241, 0.74)'), // dark blue
+    new ContentLink('Where can I work?', 'What kinds of careers does SYDE prepare me for?',
+      '#', 'rgba(255, 149, 0, 0.74)'), // amber
+    new ContentLink('Student creations', 'Can I see some projects/businesses that SYDE students have created?',
+      '#', 'rgba(88, 86, 214, 0.74)'), // dark purple
+    new ContentLink('What next?', 'How to prep the summer before, and where to talk to current students.',
+      '#', 'rgba(216, 27, 96, 0.74)'), // pink
   ];
 
   constructor(private router: Router,
@@ -33,7 +48,7 @@ export class ContentLink {
   description: string;
   color: string;
 
-  constructor(_header: string, _link: string, _description: string, _color: string) {
+  constructor(_header: string, _description: string, _link: string, _color: string) {
     this.header = _header;
     this.link = _link;
     this.description = _description;
