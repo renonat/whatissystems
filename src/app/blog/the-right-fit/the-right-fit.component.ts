f1970af131541bd763a21491f3806a9aa68fedc6
Reno Natalizio <renonatalizio@gmail.com>
2017-10-31 20:44:37 -0400

    Use the frappe-charts js library for displaying chart data
    Add charts to the the-right-fit page

--- src/app/blog/the-right-fit/the-right-fit.component.ts
+++ src/app/blog/the-right-fit/the-right-fit.component.ts
@@ -2,11 +2,13 @@ import {
   Component, OnInit,
 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ChartService } from '../../services/ChartService';
 
 @Component({
   selector: 'post-the-right-fit',
   styleUrls: ['the-right-fit.component.scss'],
-  templateUrl: 'the-right-fit.component.html'
+  templateUrl: 'the-right-fit.component.html',
+  providers: [ChartService]
 })
 export class TheRightFitComponent implements OnInit {
   title = 'The Right Fit';
@@ -15,12 +17,14 @@ export class TheRightFitComponent implements OnInit {
     return 'the-right-fit';
   }
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title,
+              private chartService: ChartService) {
 
   }
 
   ngOnInit(): void {
     this.titleService.setTitle(this.title);
+    this.chartService.theRightFitOnLoad();
   }
 
 }
---
+++ src/app/services/ChartService.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+declare var Chart: any;
+
+@Injectable()
+export class ChartService {
+
+
+
+  theRightFitOnLoad(): void {
+
+    let chartNumberOfFriendsInSyde = function() {
+      let data = {
+        labels: ['1-5', '6-10', '11-15', '16-20', '20-25', '25+'],
+        datasets: [
+          {
+            values: [20, 33, 14, 16, 9, 8]
+          }
+        ]
+      };
+
+      new Chart({
+        parent: '#chart-number-of-friends-in-syde',
+        title: 'Number of Friends in SYDE (%)',
+        data: data,
+        type: 'percentage'
+      });
+    };
+
+    let chartNumberOfFiveClosestFriendsInSyde = function() {
+      let data = {
+        labels: ['0', '1', '2', '3', '4', '5'],
+        datasets: [
+          {
+            values: [8, 7, 17, 15, 18, 35]
+          }
+        ]
+      };
+
+      new Chart({
+        parent: '#chart-number-of-five-closest-friends-in-syde',
+        title: 'Number of 5 Closest Friends in SYDE (%)',
+        data: data,
+        type: 'percentage'
+      });
+    };
+
+    let chartSydeEventAttendance = function() {
+      let data = {
+        labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
+        datasets: [
+          {
+            values: [5, 3, 4, 8, 6, 7, 7, 16, 42]
+          }
+        ]
+      };
+
+      new Chart({
+        parent: '#chart-syde-event-attendance',
+        title: 'Number of terms where attended at least 1 SYDE event (%)',
+        data: data,
+        type: 'bar'
+      });
+    };
+
+    window.onload = function() {
+      chartNumberOfFriendsInSyde();
+      chartNumberOfFiveClosestFriendsInSyde();
+      chartSydeEventAttendance();
+    };
+  }
+
+}
