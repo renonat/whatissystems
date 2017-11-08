1d3bf8a8556585a0b7400616365e4e1470f38db4
Reno Natalizio <renonatalizio@gmail.com>
2017-11-07 19:51:53 -0500

    Make some UI visual changes

--- src/app/data/contents-data.ts
+++ src/app/data/contents-data.ts
@@ -23,21 +23,21 @@ export class ContentLink {
 export const TABLE_OF_CONTENTS: [ContentLink] = [
   new ContentLink('What do we learn?',
     'Our education exists at the intersection of Systems Theory and Design Thinking.',
-    '#/' + OurEducationComponent.path(), 'rgba(40, 203, 252, 0.74)'), // blue
+    '#/' + OurEducationComponent.path(), 'rgb(40, 203, 252)'), // blue
   new ContentLink('Is this a general engineering program?',
     'SYDE is its own speciality, and has great opportunities for specialization' ,
-    '#/' + GeneralEngineeringComponent.path(), 'rgba(239, 94, 111, 0.74)'), // red
+    '#/' + GeneralEngineeringComponent.path(), 'rgb(239, 94, 111)'), // red
   new ContentLink('Is this some newfangled program?', 'Learn about the history of our program.',
-    '#/' + HistoryComponent.path(), 'rgba(2, 184, 117, 0.74)'), // green
+    '#/' + HistoryComponent.path(), 'rgb(2, 184, 117)'), // green
   new ContentLink('Will this be the right fit for me?',
     'Explore what it means to be a SYDE student, and the community we have created.',
-    '#/' + TheRightFitComponent.path(), 'rgba(221, 104, 244, 0.74)'), // purple
+    '#/' + TheRightFitComponent.path(), 'rgb(221, 104, 244)'), // purple
   new ContentLink('What is co-op like?', 'Everything from 4-stream to rankings, this is how to get a job 101',
-    '#/' + CoopComponent.path(), 'rgba(70, 136, 241, 0.74)'), // dark blue
+    '#/' + CoopComponent.path(), 'rgb(70, 136, 241)'), // dark blue
   new ContentLink('Where can I work?', 'What kinds of careers does SYDE prepare me for?',
-    '#/' + CareersComponent.path(), 'rgba(255, 149, 0, 0.74)'), // amber
+    '#/' + CareersComponent.path(), 'rgb(255, 149, 0)'), // amber
   new ContentLink('Student creations', 'Can I see some projects/businesses that SYDE students have created?',
-    '#/', 'rgba(88, 86, 214, 0.74)'), // dark purple
+    '#/', 'rgb(88, 86, 214)'), // dark purple
   new ContentLink('What next?', 'How to prep the summer before, and where to talk to current students.',
-    '#/', 'rgba(216, 27, 96, 0.74)'), // pink
+    '#/', 'rgb(216, 27, 96)'), // pink
 ];
