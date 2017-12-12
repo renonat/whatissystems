abc31740a47f9642506c934c8f7fd10bca0c8b94
Reno Natalizio <renonatalizio@gmail.com>
2017-12-12 16:29:09 -0500

    Fix Education page
    Plus move all the basic html and css out to the new folders

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(favicon('public/favicon.ico'));
 
 app.get('/', (req, res) => res.sendFile('public/app/homepage.html', { root: __dirname }))
 app.get('/careers', (req, res) => res.sendFile('public/app/careers.html', { root: __dirname }))
+app.get('/co-op', (req, res) => res.sendFile('public/app/co-op.html', { root: __dirname }))
+app.get('/education', (req, res) => res.sendFile('public/app/education.html', { root: __dirname }))
+app.get('/history', (req, res) => res.sendFile('public/app/history.html', { root: __dirname }))
+app.get('/general-engineering', (req, res) => res.sendFile('public/app/general-engineering.html', { root: __dirname }))
+app.get('/projects', (req, res) => res.sendFile('public/app/projects.html', { root: __dirname }))
+app.get('/right-fit', (req, res) => res.sendFile('public/app/right-fit.html', { root: __dirname }))
+app.get('/what-next', (req, res) => res.sendFile('public/app/what-next.html', { root: __dirname }))
 
 app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
