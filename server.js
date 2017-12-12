65fff78796af8ed2b98da94f9b4bacbc7b14a6df
Reno Natalizio <renonatalizio@gmail.com>
2017-12-12 14:52:02 -0500

    Making things work
    Rejigged everything so that the homepage works well, plus other changes along the way

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+var favicon = require('serve-favicon');
 var path = require('path')
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
-app.get('/careers', (req, res) => res.sendFile('careers.component.html', { root: __dirname }))
+app.use(favicon('public/favicon.ico'));
+
+app.get('/', (req, res) => res.sendFile('public/app/homepage.html', { root: __dirname }))
+app.get('/careers', (req, res) => res.sendFile('public/app/careers.html', { root: __dirname }))
 
 app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
