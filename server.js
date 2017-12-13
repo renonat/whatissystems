5c2b198d2f8475ed73271cae8d416b9f2b7cc669
Reno Natalizio <renonatalizio@gmail.com>
2017-12-12 21:20:18 -0500

    Add custom pre-processing templater for html

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(favicon('public/favicon.ico'));
 
-app.get('/', (req, res) => res.sendFile('public/app/homepage.html', { root: __dirname }))
-app.get('/careers', (req, res) => res.sendFile('public/app/careers.html', { root: __dirname }))
-app.get('/co-op', (req, res) => res.sendFile('public/app/co-op.html', { root: __dirname }))
-app.get('/education', (req, res) => res.sendFile('public/app/education.html', { root: __dirname }))
-app.get('/history', (req, res) => res.sendFile('public/app/history.html', { root: __dirname }))
-app.get('/general-engineering', (req, res) => res.sendFile('public/app/general-engineering.html', { root: __dirname }))
-app.get('/projects', (req, res) => res.sendFile('public/app/projects.html', { root: __dirname }))
-app.get('/right-fit', (req, res) => res.sendFile('public/app/right-fit.html', { root: __dirname }))
-app.get('/what-next', (req, res) => res.sendFile('public/app/what-next.html', { root: __dirname }))
+app.get('/', (req, res) => res.sendFile('public/html/homepage.html', { root: __dirname }))
+app.get('/careers', (req, res) => res.sendFile('public/html/careers.html', { root: __dirname }))
+app.get('/co-op', (req, res) => res.sendFile('public/html/co-op.html', { root: __dirname }))
+app.get('/education', (req, res) => res.sendFile('public/html/education.html', { root: __dirname }))
+app.get('/history', (req, res) => res.sendFile('public/html/history.html', { root: __dirname }))
+app.get('/general-engineering', (req, res) => res.sendFile('public/html/general-engineering.html', { root: __dirname }))
+app.get('/projects', (req, res) => res.sendFile('public/html/projects.html', { root: __dirname }))
+app.get('/right-fit', (req, res) => res.sendFile('public/html/right-fit.html', { root: __dirname }))
+app.get('/what-next', (req, res) => res.sendFile('public/html/what-next.html', { root: __dirname }))
 
 app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
