dc0383bf3037ca74a2749a8986dd2f42fc45f06c
renonat <renonatalizio@gmail.com>
2018-02-17 16:05:11 -0500

    Use boostrap navbar
    Strip many of the custom theming, in order to have a base responsive navbar

--- public/js/header.js
+++ public/js/header.js
@@ -1,26 +1,3 @@
-// Document listener for dropdown clicks, to expand/hide dropdown for learn links
-document.addEventListener('click', function(event) {
-  var dropdown = document.getElementById('dropdown-links');
-  var isClickInDropdown = dropdown.contains(event.target);
-  var isClickOnButton = document.getElementById('dropdown-button').contains(event.target);
-
-  // If is click in dropdown, ignore click event
-  if (!isClickInDropdown) {
-    if (isClickOnButton) {
-        // Clicks on the button toggle the dropdown
-        if (dropdown.className.includes(' show')) {
-            dropdown.className = dropdown.className.replace(' show', '');
-        } else {
-            dropdown.className += ' show';
-        }
-    } else {
-        // Clicks outside of the dropdown hide the dropdown
-        dropdown.className = dropdown.className.replace(' show', '');
-    }
-  }
-
-});
-
 // Ordinating numbers: http://stackoverflow.com/questions/15809950/javascript-ordinal-suffix-for-numbers-with-specific-css-class
 ordinate = function(num){
     var /*num = this,*/
